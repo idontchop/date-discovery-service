579ec6500a9e6632967da9043484162c412c8aee
Nate <nate@idontchop.com>
2020-03-16 16:51:57 -0700

    Initial Commit

---
+++ src/test/java/com/idontchop/dateDiscovery/DateDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idontchop.dateDiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DateDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
